3220a72ff5346f9eb0050e6ade92b22ac8c2e1cc
Cao Tan Duc <89432129+caotanduc@users.noreply.github.com>
2025-11-04 23:16:04 +0700

    update mode-line, dashboard closing behaviour and column-indicator-line

--- .emacs.d/lisp/dashboard.el
+++ .emacs.d/lisp/dashboard.el
@@ -1,7 +1,6 @@
-(require 'vscode)
-
 (defun my/welcome-buffer ()
-  "Display a centered 'fakeide' logo: 'fake' (yellow on black), 'ide' (black on yellow)."
+  "Display a centered 'fakeide' logo: 'fake' (yellow on black), 'ide' (black on yellow).
+Close the buffer when any key is pressed."
   (interactive)
   (let ((buf (get-buffer-create "*Welcome*"))
         (inhibit-read-only t))
@@ -23,15 +22,21 @@
         (insert (make-string (max 0 v-padding) ?\n))
         (insert (make-string (max 0 h-padding) ?\s))
         (insert text))
-      (vscode-mode)
       ;; Make buffer unscrollable and read-only
       (setq buffer-read-only t)
       (setq-local window-size-fixed 'both)
       (local-set-key [wheel-up] #'ignore)
       (local-set-key [wheel-down] #'ignore)
       (local-set-key (kbd "<up>") #'ignore)
-      (local-set-key (kbd "<down>") #'ignore))
-    (switch-to-buffer buf)))
+      (local-set-key (kbd "<down>") #'ignore)
 
+      ;; Define keymap that closes the buffer on any key
+      (use-local-map (let ((map (make-sparse-keymap)))
+                       (define-key map [t]
+                         (lambda ()
+                           (interactive)
+                           (kill-buffer "*Welcome*")))
+                       map)))
+    (switch-to-buffer buf)))
 
 (provide 'dashboard)
