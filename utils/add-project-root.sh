067661a5417090d60c19b0c768607bad1426b09d
Cao Tan Duc <89432129+caotanduc@users.noreply.github.com>
2025-11-06 23:53:55 +0700

    remove unused script

--- utils/add-project-root.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-CACHE_FILE="${XDG_CACHE_HOME:-$HOME/.cache}/tmux-projects-root"
-read -rp "Enter new project root path: " NEWROOT
-[[ -d "$NEWROOT" ]] || { echo "Invalid path."; exit 1; }
-grep -qxF "$NEWROOT" "$CACHE_FILE" 2>/dev/null || echo "$NEWROOT" >>"$CACHE_FILE"
-echo "Added: $NEWROOT"
