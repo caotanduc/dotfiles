ff65293b5368b15dfa5c7a695b678660657b27b2
Cao Tan Duc <89432129+caotanduc@users.noreply.github.com>
2025-11-25 00:03:34 +0700

    emacs add embark

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -202,6 +202,31 @@
   :init
   (marginalia-mode))
 
+;; ─── Embark ──────────────────────────────────────────────────────────────────
+(use-package embark
+  :bind
+  (("C-c ." . embark-act)         ;; pick some comfortable binding
+   ("C-c ;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+
 ;; ─── Consult ─────────────────────────────────────────────────────────────────
 (use-package consult
   :bind (;; C-x bindings in `ctl-x-map'
