4b5c83a48922efe4ba8f409a4b17bd9f9ae4f869
Duc Cao <ductancao.work@gmail.com>
2026-02-09 09:21:31 +0700

    zsh add aws session manager function

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -11,3 +11,44 @@ fh() {
 fkill() {
   ps -ef | sed 1d | fzf | awk '{print $2}' | xargs kill -9
 }
+
+ssm() {
+  local profile="${1:-bruno}"
+  local region="${2:-ap-southeast-1}"
+
+  # SSO login (will only prompt if needed)
+  aws sso login --profile "$profile" >/dev/null
+
+  # Get instances as JSON
+  local json
+  json="$(
+    aws ec2 describe-instances \
+      --profile "$profile" --region "$region" \
+      --filters "Name=tag:allow,Values=ssm" "Name=instance-state-name,Values=running" \
+      --output json
+  )" || return 1
+
+  # Use jq to build pickable rows, fzf to choose one, then extract instance id
+  local target
+  target="$(
+    jq -r '
+      .Reservations[].Instances[]
+      | {
+          id: .InstanceId,
+          name: ((.Tags // []) | map(select(.Key=="Name") | .Value) | .[0] // "-"),
+          ip: (.PrivateIpAddress // "-"),
+          type: (.InstanceType // "-"),
+          az: (.Placement.AvailabilityZone // "-")
+        }
+      | "\(.id)\t\(.name)\t\(.ip)\t\(.type)\t\(.az)"
+    ' <<<"$json" \
+    | sort -k2,2 \
+    | fzf --with-nth=2.. --delimiter=$'\t' --prompt="SSM target > " \
+    | cut -f1
+  )"
+
+  [[ -z "$target" ]] && return 1
+
+  aws ssm start-session --profile "$profile" --region "$region" --target "$target"
+}
+
